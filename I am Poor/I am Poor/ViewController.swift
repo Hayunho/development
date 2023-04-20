3d0b5ae7eea1ed047da10d7632a1dc81c6e58044
Hayunho <yunho081544@gmail.com>
2023-04-21 03:28:32 +0900

    Complete 'I am Poor'

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by 하윤호 on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
