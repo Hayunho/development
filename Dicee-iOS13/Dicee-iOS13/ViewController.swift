34d4dba76e803f5f9292554c041166a57cc7334a
Hayunho <yunho081544@gmail.com>
2023-04-22 02:48:59 +0900

    swift 연습장 추가, 버튼 클릭시 주사위 변경

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,25 +13,42 @@ class ViewController: UIViewController {
     //IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //  WHO         WHAT       VALUE
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
-        // #imageLiteral() 로 하면 강의 영상처럼 이미지를 찾아서 넣을 수 있음
-        
-        //challenge:
-        //diceImageView1.alpha = 0.5
+//        //  WHO         WHAT       VALUE
+//        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+//        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+//        // #imageLiteral() 로 하면 강의 영상처럼 이미지를 찾아서 넣을 수 있음
+//
+//        //challenge:
+//        //diceImageView1.alpha = 0.5
         
         
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
+        print("leftDiceNumber at beginning = \(leftDiceNumber)")
+        print("rightDiceNumber at beginning = \(rightDiceNumber)")
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceSix")
+        //                          [0, 1, 2, 3, 4, 5][몇 번째?]
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+        if (leftDiceNumber >= 6) {
+            leftDiceNumber = 0
+        }
+        if (rightDiceNumber < 0) {
+            rightDiceNumber = 5
+        }
+        
+        print("leftDiceNumber at end = \(leftDiceNumber)")
+        print("rightDiceNumber at end = \(rightDiceNumber)")
     }
     
 }
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// This is a comment
+// print("Hello World")
+
+print("Hello \(2 + 3) World")
+print("The result of 5 + 3 = \(5 + 3)")
