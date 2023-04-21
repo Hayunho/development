a3a130ced6b02048c05bda216c4e3f1762f758b6
Hayunho <yunho081544@gmail.com>
2023-04-22 02:01:18 +0900

    이미지 변경 코드 작성

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //  WHO         WHAT       VALUE
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        // #imageLiteral() 로 하면 강의 영상처럼 이미지를 찾아서 넣을 수 있음
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
+        //challenge:
+        //diceImageView1.alpha = 0.5
+        
+        
     }
 
 
